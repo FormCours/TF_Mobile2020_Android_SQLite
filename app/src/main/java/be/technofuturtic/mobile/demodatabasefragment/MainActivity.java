bcb68669a11983a6fca2de4b6f86de85642db0b0
Formation_TFTIC <forma300@tftic.be>
2021-01-19 15:39:20 +0100

    Add layout on MainActivity

--- app/src/main/java/be/technofuturtic/mobile/demodatabasefragment/MainActivity.java
+++ app/src/main/java/be/technofuturtic/mobile/demodatabasefragment/MainActivity.java
@@ -3,37 +3,36 @@ package be.technofuturtic.mobile.demodatabasefragment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import be.technofuturtic.mobile.demodatabasefragment.db.dao.RecipeDao;
 import be.technofuturtic.mobile.demodatabasefragment.models.Recipe;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+    implements View.OnClickListener {
+
+    Button btnNewRecipe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RecipeDao recipeDao = new RecipeDao(this);
+        btnNewRecipe = findViewById(R.id.btn_main_new);
+        btnNewRecipe.setOnClickListener(this);
 
-        /*
-        // Ajout d'un element en DB
-        Recipe recipe = new Recipe("Demo");
+    }
 
-        recipeDao.openWritable();
-        recipeDao.create(recipe);
-        recipeDao.close();
-        */
-
-        /*
-        // Obtenir un element via son ID
-        recipeDao.openReadable();
-        Recipe recup = recipeDao.get(1);
-        recipeDao.close();
-
-        Toast.makeText(this, "Recette : " + recup.getName(), Toast.LENGTH_LONG).show();
-         */
-
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_main_new:
+                // TODO Faire l'ajout
+                break;
+            default:
+                throw new RuntimeException(String.format("Click not support on %s", v.toString()));
+        }
     }
 }
